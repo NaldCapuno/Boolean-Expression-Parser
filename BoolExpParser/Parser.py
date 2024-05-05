6d77c5834f2d8501f95295aea7e61d127985f77a
NaldCapuno <jasoncapuno@gmail.com>
2024-05-05 20:45:20 +0800

    fixed one problem on sympy

--- BoolExpParser/Parser.py
+++ BoolExpParser/Parser.py
@@ -1,10 +1,20 @@
 from sympy.logic import simplify_logic
+import time, os
 
 def lexer(expression):
     tokens = []
     position = 0
     while position < len(expression):
         if expression[position] == " ":
+            position += 1
+    
+        elif expression[position].isalnum() and position < len(expression)-1  and (expression[position+1].isalnum() or expression[position+1] == "!" or expression[position+1] == "("):
+            tokens.append(expression[position])
+            tokens.append("&")
+            position += 1
+
+        elif expression[position].isalnum():
+            tokens.append(expression[position])
             position += 1
 
         elif expression[position] == "(":
@@ -27,10 +37,6 @@ def lexer(expression):
             tokens.append(expression[position])
             position += 1
 
-        elif expression[position].isalnum():
-            tokens.append(expression[position])
-            position += 1
-   
     return tokens
 
 def parser(tokens):
@@ -66,11 +72,16 @@ def parser(tokens):
     exp_for_simp = ''.join(exp_for_simp)
     return simplify_logic(exp_for_simp)
 
-
 if __name__ == "__main__":
     while True:
-        expression = input("Enter Boolean Expression: ")
+        os.system('cls')
+        print("Boolean Expression Parser\n")
+        expression = input("Enter Boolean Expression (Leave blank to exit): ")
         if expression == "":
+            print("Exiting...")
+            time.sleep(1)
             break
         tokens = lexer(expression)
-        print(f"Simplified Expression: {parser(tokens)}\n")
+        simplified_exp = parser(tokens)
+        print(f"Simplified Expression: {simplified_exp}\n")
+        input("Press Enter to parse your next expression...")
